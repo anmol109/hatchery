2de2780e78ec3553afa7da14c7c78c8e16f60cf5
anmol109 <67062356+anmol109@users.noreply.github.com>
2020-06-19 00:53:15 +0530

    Add files via upload

--- resultGenerator.js
+++ resultGenerator.js
@@ -1,35 +1,38 @@
-function show() {
-    var x = document.getElementById("inp-cont");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+var newv=document.querySelector("#newv");
+var subm=document.querySelector("#subm");
+var result=document.querySelector("#result");
+var subjectInput=document.querySelector("#subjectInput")
+var subName=["abc"];
+var subCode=["xyz"];
+var credits=[2];
+var sName=document.querySelector("#sName");
+var sCode=document.querySelector("#sCode");
+var credits=document.querySelector("#credits");
+var subNameDisp=document.querySelector("#sNameDisp");
+var subCodeDisp=document.querySelector("#sCodeDisp");
+var creditsDisp=document.querySelector("#creditsDisp");
+var showResult=document.querySelector("#showResult");
+var numOfSubs=0;
+
+newv.addEventListener("click", function(){
+  subjectInput.style.display = "block";
+});
+subm.addEventListener("click", function(){
+  subName[numOfSubs] = sName.value
+  subCode[numOfSubs] = sCode.value;
+  credits[numOfSubs] = credits.value;
+  numOfSubs++;
+  subjectInput.style.display= "none";
+});
+result.addEventListener("click", function(){
+  showResult.style.display=" block";
+  for(var i=0; i<numOfSubs; i++)
+  {
+    subNameDisp.textContent+=subName[i];
+    subCodeDisp.textContent+=subCode[i];
+    creditsDisp.textContent+=credits[i];
+
   }
-
-function show2() {
-    var y = document.getElementById("show-result");
-    if (y.style.display === "none") {
-        y.style.display = "block";
-    } else {
-        y.style.display = "none";
-    }
-
-}
-
-
-var subname=["maths"];
-var subcode=["mat"];
-var subcred=[2];
-function sub(){
-    subname.push(getElementById("name").value());
-    subcode.push(getElementById("code").value());
-    subcred.push(getElementById("number").value());
-}
-
-function showresult(){
-    document.getElementById("subject-name").innerHTML = subname[1];
-    document.getElementById("gpa").innerHTML = subcred[0]
-}
+});
 
 
