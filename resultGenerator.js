4c12ca51d11cedf84ab0872bed8694799c86c640
Kingshuk Mukherjee <56651753+kingshuk01@users.noreply.github.com>
2020-06-25 23:13:45 +0530

    Add files via upload

--- resultGenerator.js
+++ resultGenerator.js
@@ -2,11 +2,11 @@ var newv=document.querySelector("#newv");
 var subm=document.querySelector("#subm");
 var result=document.querySelector("#result");
 var subjectInput=document.querySelector("#subjectInput");
-var GPAGrade=document.querySelector("#grade");
+var Grade=document.querySelector("#grade");
 var subName=[];
 var subCode=[];
 var credits=[];
-var GPA=[];
+var SubGrade=[];
 var sName=document.querySelector("#sName");
 var sCode=document.querySelector("#sCode");
 var credits=document.querySelector("#credits");
@@ -14,8 +14,11 @@ var subNameDisp=document.querySelector("#sNameDisp");
 var subCodeDisp=document.querySelector("#sCodeDisp");
 var creditsDisp=document.querySelector("#creditsDisp");
 var showResult=document.querySelector("#showResult");
-var GPADisp=document.querySelector("#GPADisp");
+var GradeDisp=document.querySelector("#GPADisp");
+var GPADisp=document.querySelector("#GPA");
 var numOfSubs=0;
+var gpa=0;
+var cred=0;
 
 newv.addEventListener("click", function(){
   subjectInput.style.display = "block";
@@ -24,7 +27,7 @@ subm.addEventListener("click", function(){
   subName[numOfSubs] = sName.value
   subCode[numOfSubs] = sCode.value;
   credits[numOfSubs] = credits.value;
-  GPA[numOfSubs] = GPAGrade.value;
+  SubGrade[numOfSubs] = Grade.value;
   numOfSubs++;
   subjectInput.style.display= "none";
 });
@@ -35,8 +38,34 @@ result.addEventListener("click", function(){
     subNameDisp.innerHTML+=subName[i]+"&#13;&#10;";
     subCodeDisp.innerHTML+=subCode[i]+"&#13;&#10;";
     creditsDisp.innerHTML+=credits[i]+"&#13;&#10;";
-    GPADisp.innerHTML+=GPA[i]+"&#13;&#10;";
+    GradeDisp.innerHTML+=SubGrade[i]+"&#13;&#10;";
+    gpa+=tell(SubGrade[i])*credits[i];
+    cred+=credits[i];
   }
+  GPADisp.value=gpa/cred;
 });
 
+function tell(grade){
+  if(grade=='A'){
+    return 9;
+  }
+  else if(grade=='B'){
+    return 8;
+  }
+  else if(grade=='C'){
+    return 7;
+  }
+  else if(grade=='D'){
+    return 6;
+  }
+  else if(grade=='E'){
+    return 5;
+  }
+  else if(grade=='S'){
+    return 10;
+  }
+  else if(grade=='N'){
+    return 0;
+  }
+}
 
